64b442593a1e3e02dd9f84293c73ac37f175203b
Ajay Ratnam <ajayratnam.satheesh@gmail.com>
2021-10-03 14:56:59 +0530

    Variable assignment bug

--- lang.py
+++ lang.py
@@ -4,17 +4,17 @@ while True:
     inp = input(">>").lstrip()
     variable_assigned = False
 
-    # variable handler
-    if ":=" in inp:
-        var_name, var_value, *_ = inp.split(":=")
-        var_name = var_name.strip()
-        if ' ' not in var_name and var_value:
-            variables[var_name] = var_value
-            variable_assigned = True
-
     # replace variable names with values
     for var_name, var_value in variables.items():
         inp = inp.replace(f"{var_name}$", var_value)
+
+    # variable handler
+    if ":=" in inp:
+        var_name, *var_value = inp.split(":=")
+        var_name, var_value = var_name.strip(), ':='.join(var_value)
+        if ' ' not in var_name and var_value:
+            variables[var_name] = var_value
+            variable_assigned = True
 
     inp = inp or "No content"
 
